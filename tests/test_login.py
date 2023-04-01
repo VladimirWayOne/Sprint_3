fa576d2dd7c3e7468bd231598dfdc4b10cf68bc0
VladimirWayOne <rekin.vladimir@yandex.ru>
2023-04-01 15:06:28 +0300

    Update test_login.py

--- tests/test_login.py
+++ tests/test_login.py
@@ -3,6 +3,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 import json
+import pytest
 
 
 class TestStellarBurgersLoginForm:
@@ -39,12 +40,54 @@ class TestStellarBurgersLoginForm:
         driver.find_element(By.XPATH, ".//a[text()='Войти']").click()
         assert driver.current_url == 'https://stellarburgers.nomoreparties.site/login'
 
+    def test_login_empty_email_and_password_nothing_happens(self, driver):
+        """При нажатии на кнопку 'Войти' с пустым логином и паролем ничего не происходит"""
+        driver.get('https://stellarburgers.nomoreparties.site/login')
+        # DOM до нажатия на кнопку 'Войти'
+        old_DOM = driver.find_elements(By.XPATH, ".//*")
+        driver.find_element(By.XPATH, ".//button[text()='Войти']").click()
+        # DOM после нажатия на кнопку 'Войти'
+        new_DOM = driver.find_elements(By.XPATH, ".//*")
+        assert old_DOM == new_DOM
+
     def test_login_incorrect_email_nothing_happens(self, driver):
         """При вводе некорректного логина ничего не происходит"""
         driver.get('https://stellarburgers.nomoreparties.site/login')
+        # Локатор Email
+        driver.find_element(By.XPATH,
+                            ".//label[text()='Email']//parent::*/input[@type='text' and @name='name']").send_keys('qwe')
+        # DOM до нажатия на кнопку 'Войти'
         old_DOM = driver.find_elements(By.XPATH, ".//*")
         driver.find_element(By.XPATH, ".//button[text()='Войти']").click()
+        # DOM после нажатия на кнопку 'Войти'
         new_DOM = driver.find_elements(By.XPATH, ".//*")
         assert old_DOM == new_DOM
+
+    def test_login_incorrect_password_six_symbols_nothing_happens(self, driver):
+        """При вводе некорректного пароля длинной 6 символов ничего не происходит"""
+        driver.get('https://stellarburgers.nomoreparties.site/login')
+        # Локатор Email
+        driver.find_element(By.XPATH,
+                            ".//label[text()='Email']//parent::*/input[@type='text' and @name='name']").send_keys('qwe')
+        driver.find_element(By.XPATH, ".//input[@type='password' and @name='Пароль']").send_keys("123456")
+        # DOM до нажатия на кнопку 'Войти'
+        old_DOM = driver.find_elements(By.XPATH, ".//*")
+        driver.find_element(By.XPATH, ".//button[text()='Войти']").click()
+        # DOM после нажатия на кнопку 'Войти'
+        new_DOM = driver.find_elements(By.XPATH, ".//*")
+        assert old_DOM == new_DOM
+
+    @pytest.mark.parametrize('password_list', ['1', '12345'])
+    def test_login_incorrect_password_less_six_symbols_show_error(self, driver, password_list):
+        """При вводе некорректного пароля длинной менее 6 символов отображает ошибку 'Некорректный пароль'"""
+        driver.get('https://stellarburgers.nomoreparties.site/login')
+        # Локатор Email
+        driver.find_element(By.XPATH,
+                            ".//label[text()='Email']//parent::*/input[@type='text' and @name='name']").send_keys('qwe')
+        driver.find_element(By.XPATH, ".//input[@type='password' and @name='Пароль']").send_keys(password_list)
+        driver.find_element(By.XPATH, ".//button[text()='Войти']").click()
+        # Находим сообщение об ошибке
+        error_message = driver.find_element(By.XPATH, ".//p[contains(@class, 'input__error')]")
+        assert error_message.text == 'Некорректный пароль'
 
 
