c8a72e209b0f0911d3b7b53eb35812a38ef7b8cc
VladimirWayOne <rekin.vladimir@yandex.ru>
2023-04-02 14:50:39 +0300

    Add urls

--- tests/urls.py
+++ tests/urls.py
@@ -3,3 +3,4 @@ class Urls:
     url_login = "https://stellarburgers.nomoreparties.site/login"   # Ссылка на страницу входа
     url_profile = "https://stellarburgers.nomoreparties.site/account/profile"   # Ссылка формы Личный кабинет -> Профиль
     url_register = "https://stellarburgers.nomoreparties.site/register"     # Ссылка на страницу регистрации
+    url_forgot_password = "https://stellarburgers.nomoreparties.site/forgot-password"
